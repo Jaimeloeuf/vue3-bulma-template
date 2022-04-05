6afa3e686023682d8001d574cf2f12a11d1403c5
Jaime <jaimeloeuf@gmail.com>
2022-04-05 21:37:11 +0800

    Update state to follow pinia docs.

--- src/store.js
+++ src/store.js
@@ -4,13 +4,11 @@ import { defineStore } from "pinia";
 // the first argument is a unique id of the store across your application
 export const useStore = defineStore("main", {
   // arrow function recommended for full type inference
-  state: () => {
-    return {
-      // all these properties will have their type inferred automatically
-      name: "User",
-      isAdmin: true,
-    };
-  },
+  state: () => ({
+    // all these properties will have their type inferred automatically
+    name: "User",
+    isAdmin: true,
+  }),
 
   // https://pinia.vuejs.org/core-concepts/actions.html
   actions: {},
