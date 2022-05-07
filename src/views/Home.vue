794cc9517d0b010d65ba6b29f5adac21a52a2faa
Jaime <jaimeloeuf@gmail.com>
2022-05-07 22:36:14 +0800

    Update Home view to use both Demo components.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,88 @@
 <template>
   <!--
-    Pass timeValue as a prop to child component.
-    More specifically, timeValue is binded to child component,
-    meaning that the prop value the child component receives is
-    automatically updated as the `timeValue` here gets updated.
+    Component selector tab is wrapped in a sticky navbar.
+    See also the class applied to the html tag in index.html
   -->
-  <Demo :timeAsProp="timeValue">
+  <nav class="navbar is-fixed-top">
+    <div class="tabs is-centered">
+      <ul>
+        <li
+          :class="{ 'is-active': API_type === 'Options' }"
+          @click="API_type = 'Options'"
+        >
+          <a>Options API</a>
+        </li>
+
+        <li
+          :class="{ 'is-active': API_type === 'Composition' }"
+          @click="API_type = 'Composition'"
+        >
+          <a>Composition API</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <!--
+      Dynamic component used based on the tab selected
+      Reference: https://vuejs.org/guide/essentials/component-basics.html#dynamic-components
+      
+      Pass timeValue as a prop to child component.
+      More specifically, timeValue is binded to child component,
+      meaning that the prop value the child component receives is
+      automatically updated as the `timeValue` here gets updated.
+    -->
+  <component :is="'Demo' + API_type" :timeAsProp="timeValue">
     <!-- Example on how to use a named slot -->
     <template #title>
-      <p class="title">Vue3 Bulma Template</p>
+      <p class="title">Vue3 Bulma Template ({{ API_type }})</p>
     </template>
-  </Demo>
+  </component>
+
+  <!-- Alternative using simple if/else vue directives instead of dynamic components -->
+  <!--
+    <div v-if="API_type === 'options'">
+      <DemoOptions :timeAsProp="timeValue">
+        <template #title>
+          <p class="title">Vue3 Bulma Template (Options)</p>
+        </template>
+      </DemoOptions>
+    </div>
+    <div v-else-if="API_type === 'composition'">
+      <DemoComposition :timeAsProp="timeValue">
+        <template #title>
+          <p class="title">Vue3 Bulma Template (Composition)</p>
+        </template>
+      </DemoComposition>
+    </div>
+    <div v-else class="section">
+      <h1 class="title">INTERNAL ERROR: Invalid API type selected</h1>
+    </div>
+  -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
-import Demo from "../components/Demo.vue";
+import DemoOptions from "../components/DemoOptions.vue";
+import DemoComposition from "../components/DemoComposition.vue";
 
 export default defineComponent({
   name: "Home",
 
-  components: { Demo },
+  components: { DemoOptions, DemoComposition },
 
-  data() {
-    return { timeValue: Date.now() };
+  data(): {
+    API_type: "Options" | "Composition";
+    timeValue: number;
+  } {
+    return {
+      // Default to show Demo component built using Options API
+      // API_type: "Options",
+      API_type: "Composition",
+
+      timeValue: Date.now(),
+    };
   },
 
   created() {
