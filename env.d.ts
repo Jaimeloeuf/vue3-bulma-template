b3d5c9c72737d1693c0f5c85fdd78051317ed122
Jaime <jaimeloeuf@gmail.com>
2022-05-09 09:11:26 +0800

    Add type declaration file for .env file.

---
+++ env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vite/client" />
+
+// File to define types of env variables defined in the .env file
+// By doing this, your source files will get type information for env variables.
+// Location of this file is defined in tsconfig.json
