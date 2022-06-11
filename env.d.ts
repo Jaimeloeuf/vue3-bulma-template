9b3c51cd90111fdd97cd4ec7120dc41f393c3bd3
Jaime <jaimeloeuf@gmail.com>
2022-06-11 12:53:58 +0800

    Add type definition to env.d.ts:     Add type definition for the included base URL value and scaffold     for other values to be easily included.

--- env.d.ts
+++ env.d.ts
@@ -3,3 +3,14 @@
 // File to define types of env variables defined in the .env file
 // By doing this, your source files will get type information for env variables.
 // Location of this file is defined in tsconfig.json
+
+interface ImportMetaEnv {
+  // Allows user to set VITE_API_URL in a .env file and use `oof._baseUrl = import.meta.env.VITE_API_URL;` in main.ts
+  readonly VITE_API_URL: string;
+
+  // Add more env variables as needed...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
