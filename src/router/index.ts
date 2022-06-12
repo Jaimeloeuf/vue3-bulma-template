16c025ca5b78d800cefb68f161fa9c3a63e8e0ec
Jaime <jaimeloeuf@gmail.com>
2022-06-12 20:59:00 +0800

    Add instructions for using history mode instead.

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,9 @@ import Home from "../views/Home.vue";
 export default createRouter({
   history: createWebHashHistory(),
 
+  // Alternatively, import 'createWebHistory' to use history mode instead
+  // history: createWebHistory(),
+
   // Always scroll to top of view on first visit and no savedPosition, else reuse savedPosition
   scrollBehavior(_to, _from, savedPosition) {
     if (savedPosition) return savedPosition;
