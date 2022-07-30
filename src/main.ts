6a04fe6c446167122930be0ce2ea4d8af49edbdd
Jaime <jaimeloeuf@gmail.com>
2022-07-30 19:48:13 +0800

    Refactor API library's base URL setting logic:     To make it more generic for different type of build modes and added     docs to explain the reasoning.

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,17 @@ import "bulma/css/bulma.min.css";
 // Import API library first to set default base URL before it is used anywhere else
 import { oof } from "simpler-fetch";
 
-// If you would like to commit your base API URL with source code, use this to specify different URLs
-oof._baseUrl =
-  import.meta.env.MODE === "production"
-    ? "https://api.example.com"
-    : "http://localhost:3000";
-
-// Else use this to read a single URL from the .env file
+// If you would like to use this to read a single URL from the .env file
 // oof._baseUrl = import.meta.env.VITE_API_URL;
+
+// Else commit your base API URL with source code, use this to specify different URLs
+oof._baseUrl =
+  // Since there are modes other than production that should use the real API, such as
+  // when deploying to github-pages, the local development API is only used when running
+  // the project in development mode with `npx vite` or `npx vite build --mode development`
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : "https://api.example.com";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
