35a672da903bc6a32ab3ae5d805ced356151af24
Jaime <jaimeloeuf@gmail.com>
2022-09-19 20:17:23 +0800

    Remove need for wrapper elements in views: 1.  Remove the use of inherited class in App.vue by wrapping the entire     router element in a div rather than requiring the child components     to take care of inheriting the class, which will remove alot of     duplicated code across all child components. 2.  Update Home.vue to remove div wrapping.

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,29 @@ import Navbar from "./components/Navbar.vue";
   <Navbar />
 
   <!-- Center router view element horizontally -->
+  <div class="container">
+    <router-view />
+  </div>
+
   <!--
+    Alternative using fragment:
+
+    <router-view class="container" />
+
     By passing this attribute to child components, the child components
     cannot be a fragment (return multiple root nodes) as vue js will
     not know which node to bind the fallthrough attribute to.
+
+    Thus all elements of the child view is usually wrapped in a div even
+    though vue3 allows for multiple elements per template without a root
+    element because vue will not know which sub element to apply that class
+    onto, therefore, by grouping all under a div tag, the class applied
+    on router-view element will be inherited on the wrapper div.
 
     See reference:
     https://v3-migration.vuejs.org/new/fragments.html#overview
     https://vuejs.org/guide/components/attrs.html#fallthrough-attributes
   -->
-  <router-view class="container" />
 </template>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,73 +1,65 @@
 <template>
-  <!--
-    Wrapping all elements of this view in a div even though vue3 allows
-    for multiple elements per template without a root element.
-    However, since there is a class applied to the router-view element
-    in App.vue, vue will not know which sub element to apply that class
-    onto, therefore, by grouping all under a div tag, the class applied
-    on router-view element in App.vue will be inherited here on this div.
-  -->
-  <div>
-    <div class="section mx-3 py-0">
-      <div class="columns">
-        <div class="column">
-          <button
-            class="button is-light is-fullwidth"
-            :class="{ 'is-success': API_type === 'Options' }"
-            @click="API_type = 'Options'"
-          >
-            Options API
-          </button>
-        </div>
-        <div class="column">
-          <button
-            class="button is-light is-fullwidth"
-            :class="{ 'is-success': API_type === 'Composition' }"
-            @click="API_type = 'Composition'"
-          >
-            Composition API
-          </button>
-        </div>
+  <div class="section mx-3 py-0">
+    <div class="columns">
+      <div class="column">
+        <button
+          class="button is-light is-fullwidth"
+          :class="{ 'is-success': API_type === 'Options' }"
+          @click="API_type = 'Options'"
+        >
+          Options API
+        </button>
+      </div>
+      <div class="column">
+        <button
+          class="button is-light is-fullwidth"
+          :class="{ 'is-success': API_type === 'Composition' }"
+          @click="API_type = 'Composition'"
+        >
+          Composition API
+        </button>
       </div>
     </div>
-
-    <!--
-      Dynamic component used based on the tab selected
-      Reference: https://vuejs.org/guide/essentials/component-basics.html#dynamic-components
-      
-      Pass timeValue as a prop to child component.
-      More specifically, timeValue is binded to child component,
-      meaning that the prop value the child component receives is
-      automatically updated as the `timeValue` here gets updated.
-    -->
-    <component :is="'Demo' + API_type" :timeAsProp="timeValue">
-      <!-- Example on how to use a named slot -->
-      <template #title>
-        <p class="title mx-3">Vue3 Bulma Template ({{ API_type }})</p>
-      </template>
-    </component>
-
-    <!-- Alternative using simple if/else vue directives instead of dynamic components -->
-    <!--
-      <div v-if="API_type === 'options'">
-        <DemoOptions :timeAsProp="timeValue">
-          <template #title>
-            <p class="title">Vue3 Bulma Template (Options)</p>
-          </template>
-        </DemoOptions>
-      </div>
-      <div v-else-if="API_type === 'composition'">
-        <DemoComposition :timeAsProp="timeValue">
-          <template #title>
-            <p class="title">Vue3 Bulma Template (Composition)</p>
-          </template>
-        </DemoComposition>
-      </div>
-      <div v-else class="section">
-        <h1 class="title">INTERNAL ERROR: Invalid API type selected</h1>
-      </div>
-    -->
   </div>
+
+  <!--
+    Dynamic component used based on the tab selected
+    Reference: https://vuejs.org/guide/essentials/component-basics.html#dynamic-components
+    
+    Pass timeValue as a prop to child component.
+    More specifically, timeValue is binded to child component,
+    meaning that the prop value the child component receives is
+    automatically updated as the `timeValue` here gets updated.
+  -->
+  <component :is="'Demo' + API_type" :timeAsProp="timeValue">
+    <!-- Example on how to use a named slot -->
+    <template #title>
+      <p class="title mx-3">Vue3 Bulma Template ({{ API_type }})</p>
+    </template>
+  </component>
+
+  <!-- Alternative using simple if/else vue directives instead of dynamic components -->
+  <!-- 
+    <!--
+  <!-- 
+  <div v-if="API_type === 'options'">
+    <DemoOptions :timeAsProp="timeValue">
+      <template #title>
+        <p class="title">Vue3 Bulma Template (Options)</p>
+      </template>
+    </DemoOptions>
+  </div>
+  <div v-else-if="API_type === 'composition'">
+    <DemoComposition :timeAsProp="timeValue">
+      <template #title>
+        <p class="title">Vue3 Bulma Template (Composition)</p>
+      </template>
+    </DemoComposition>
+  </div>
+  <div v-else class="section">
+    <h1 class="title">INTERNAL ERROR: Invalid API type selected</h1>
+  </div>
+  -->
 </template>
 
 <script lang="ts">
