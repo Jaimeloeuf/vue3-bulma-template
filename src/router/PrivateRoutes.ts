73bf6661d8ab7aa217b2bd9f33d426858a2ca0c2
Jaime <jaimeloeuf@gmail.com>
2022-12-19 09:01:16 +0800

    Update const assertion syntax in Router:     To make it more readable and obvious that the Route Object is using     const assertion for its type by placing it on the same line as the     Route Object name binding.

--- src/router/PrivateRoutes.ts
+++ src/router/PrivateRoutes.ts
@@ -6,17 +6,18 @@ import { AuthType } from "./AuthType";
   Define all Private Route Objects here, before adding them to the
   type checking array defined at the bottom of this module.
 
-  `as const` needed for TS to use strict types, and name property's
-  string value as the property's string literal type, so that it
-  can be properly inferred in `RouteNames` module.
+  const assertion is needed to tell TS to use stricter types, by
+  treating string types like the `name` property as a string literal
+  type instead of a string type (ie dont widen any types), so that
+  it can be properly inferred in `RouteNames` module.
 */
 
-export const PrivateRoute = {
+export const PrivateRoute = <const>{
   path: "/private-route",
   name: "private-route",
   component: () => import("../views/DemoPrivateView.vue"),
   meta: { AuthRequirements: AuthType.Private },
-} as const;
+};
 
 /**
  * A Private route requires AuthRequirements to be specified as Private only
@@ -32,14 +33,14 @@ type PrivateRoute = RouteRecordRaw & {
  * Add all route objects defined in this file to this array for typechecking to
  * ensure that the route objects satisfies the `PrivateRoute` type constrain.
  *
- * The alternative of this is to chain the satisfies operator on every single Route
+ * The alternative of this is to use the satisfies operator on every single Route
  * Object definition, but that is more tedious and error prone as they are all done
  * individually compared to this array type checking method which makes it readable.
  *
  * Also the RouteObjects cannot simply be annotated with the `PrivateRoute` type directly
  * at the point of creation, because that will widen the RouteObject type to be that of
- * `PrivateRoute` and ignore the `as const`, which is critical for treating the `name`
- * field as a string literal type for `RouteNames` type to be properly inferred.
+ * `PrivateRoute` and ignore the const assertion, which is critical for treating the
+ * `name` field as a string literal type for `RouteNames` type to be properly inferred.
  *
  * Since this array cant be referenced and used anywhere it will be tree shaked away,
  * therefore there is no need to worry about the runtime cost of this array.
--- src/router/PublicRoutes.ts
+++ src/router/PublicRoutes.ts
@@ -6,24 +6,25 @@ import { AuthType } from "./AuthType";
   Define all Private Route Objects here, before adding them to the
   type checking array defined at the bottom of this module.
 
-  `as const` needed for TS to use strict types, and name property's
-  string value as the property's string literal type, so that it
-  can be properly inferred in `RouteNames` module.
+  const assertion is needed to tell TS to use stricter types, by
+  treating string types like the `name` property as a string literal
+  type instead of a string type (ie dont widen any types), so that
+  it can be properly inferred in `RouteNames` module.
 */
 
-export const HomeRoute = {
+export const HomeRoute = <const>{
   path: "/",
   name: "home",
   component: () => import("../views/Home.vue"),
   meta: { AuthRequirements: AuthType.Public },
-} as const;
+};
 
-export const AboutRoute = {
+export const AboutRoute = <const>{
   path: "/about",
   name: "about",
   component: () => import("../views/About.vue"),
   meta: { AuthRequirements: AuthType.Public },
-} as const;
+};
 
 /**
  * A public route requires AuthRequirements to be specified as Public or PublicOnly
@@ -39,14 +40,14 @@ type PublicRoute = RouteRecordRaw & {
  * Add all route objects defined in this file to this array for typechecking to
  * ensure that the route objects satisfies the `PublicRoute` type constrain.
  *
- * The alternative of this is to chain the satisfies operator on every single Route
+ * The alternative of this is to use the satisfies operator on every single Route
  * Object definition, but that is more tedious and error prone as they are all done
  * individually compared to this array type checking method which makes it readable.
  *
  * Also the RouteObjects cannot simply be annotated with the `PublicRoute` type directly
  * at the point of creation, because that will widen the RouteObject type to be that of
- * `PublicRoute` and ignore the `as const`, which is critical for treating the `name`
- * field as a string literal type for `RouteNames` type to be properly inferred.
+ * `PublicRoute` and ignore the const assertion, which is critical for treating the
+ * `name` field as a string literal type for `RouteNames` type to be properly inferred.
  *
  * Since this array cant be referenced and used anywhere it will be tree shaked away,
  * therefore there is no need to worry about the runtime cost of this array.
